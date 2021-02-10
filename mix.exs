9c4186a51844a8cd243a34a97c26bc287845bf4d
Pawel Antemijczuk <pawel.antemijczuk1@motorolasolutions.com>
2021-02-10 12:35:29 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TelemetryWrappers.MixProject do
   def project do
     [
       app: :telemetry_wrappers,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       docs: [extras: ["README.md"], main: "readme"],
